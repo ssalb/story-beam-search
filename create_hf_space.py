6bbaed77a6753574fe8f7c68abeec4737fc68283
ssalb <40628560+ssalb@users.noreply.github.com>
2024-12-31 12:10:48 +0100

    fix repo_id

--- create_hf_space.py
+++ create_hf_space.py
@@ -3,17 +3,18 @@ from huggingface_hub import HfApi
 from huggingface_hub.utils import HfHubHTTPError
 
 space_name = os.getenv("HF_SPACE_NAME")
+user_name = os.getenv("HF_USER_NAME")
+token = os.getenv("HF_TOKEN", None)
 
-def create_hf_space(space_name: str, private: bool = False):
+def create_hf_space(private: bool = False):
     api = HfApi()
-    token = os.getenv("HF_TOKEN", None)
     
     if token is None:
         raise ValueError("Hugging Face token not found. Set the HF_TOKEN environment variable.")
     
     try:
         # Check if the space already exists
-        api.repo_info(repo_id=space_name, repo_type="space", token=token)
+        api.repo_info(repo_id=user_name+"/"+space_name, repo_type="space", token=token)
         print(f"Space '{space_name}' already exists.")
     except HfHubHTTPError as e:
         if e.response.status_code == 404:
@@ -30,4 +31,4 @@ def create_hf_space(space_name: str, private: bool = False):
             raise e
 
 if __name__ == "__main__":
-    create_hf_space(space_name)
+    create_hf_space(private=False)
